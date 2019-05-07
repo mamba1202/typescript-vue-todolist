9b5dd61714acd469cba69ec0b4b4e669d1b79c46
mamba <1612786059@qq.com>
2019-05-07 12:34:51 +0800

    入口

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="inner">
+      <NewTodo @addTodo="addTodo"></NewTodo>
+      <TodoList :list="list"></TodoList>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import NewTodo from "./components/NewTodo.vue";
+import TodoList from "./components/TodoList.vue";
+
+interface Todo {
+  name: String;
+  status: "done" | "todo" | "deleted";
+}
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    NewTodo,
+    TodoList
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  list: Array<Todo> = [
+    { name: "任务一", status: "todo" },
+    { name: "任务二", status: "done" }
+  ];
+  addTodo(name: String) {
+    let todo: Todo = { name: name, status: "todo" }; //注意声明类型
+    this.list.push(todo);
+  }
+}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > .inner {
+    border: 1px solid grey;
+    padding: 20px;
+  }
 }
 </style>
