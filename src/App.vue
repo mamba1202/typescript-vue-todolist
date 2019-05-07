5aa8da019cce731ef48b69125358bc293daee94c
mamba <1612786059@qq.com>
2019-05-07 13:33:09 +0800

    类型

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div class="inner">
       <NewTodo @addTodo="addTodo"></NewTodo>
-      <TodoList :list="list" @updateTodo="updateTodo"></TodoList>
+      <TodoList
+        :list="list"
+        @updateTodo="updateTodo"
+      ></TodoList>
     </div>
   </div>
 </template>
@@ -11,8 +14,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import NewTodo from "./components/NewTodo.vue";
 import TodoList from "./components/TodoList.vue";
-import Todo from './models/Todo'
-
+import Todo from "./models/Todo";
 
 @Component({
   components: {
@@ -27,15 +29,17 @@ import Todo from './models/Todo'
   }
 })
 export default class App extends Vue {
-  list: Array<Todo> = localStorage.getItem('data') ? JSON.parse(<string>localStorage.getItem('data')) : []
+  list: Array<Todo> = localStorage.getItem("data")
+    ? JSON.parse(<string>localStorage.getItem("data"))
+    : [];
   addTodo(name: String) {
     let todo: Todo = { name: name, status: "todo" }; //注意声明类型
     this.list.push(todo);
   }
-  updateTodo(todo: Todo,part: Partial<Todo>){
-    let index = this.list.indexOf(todo)   //找到改变的
-    let newTodo = Object.assign({}, todo,part) //新的数据
-    this.list.splice(index,1,newTodo) //将新的数据放入
+  updateTodo(todo: Todo, part: Partial<Todo>) {
+    let index: number = this.list.indexOf(todo); //找到改变的
+    let newTodo: Todo = Object.assign({}, todo, part); //新的数据
+    this.list.splice(index, 1, newTodo); //将新的数据放入
   }
 }
 </script>
@@ -53,11 +57,12 @@ export default class App extends Vue {
 </style>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
 }
-ul,ol{
+ul,
+ol {
   list-style: none;
 }
 </style>
