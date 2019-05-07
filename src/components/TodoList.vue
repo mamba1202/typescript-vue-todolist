83e442339e018def14474ef7624df59aedde2950
mamba <1612786059@qq.com>
2019-05-07 12:34:24 +0800

    创建TodoList组件

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <ol>
+      <li v-for="(todoItem, index) in list" :key="index">
+          {{todoItem.name}}
+      </li>
+  </ol>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: {
+    list: Array
+  }
+})
+export default class NewTodo extends Vue {}
+</script>
